4bd38b71dd3e50464ccf2aed99a449d57724e75f
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-07-24 20:33:59 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,26 +10,7 @@ export default function Home() {
           <code className={styles.code}></code>
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
+          <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
@@ -37,6 +18,11 @@ export default function Home() {
           height={37}
           priority
         />
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        
 
         <iframe
           src="https://bafybeic54udbggbhtn7nfjrrj7elrim4shfped7c3cco7neset7fdwpwmu.gateway.ipfscdn.io/erc1155.html?contract=0x65D8A98596D25Df1700F6258183791E070ebD195&chain=%7B%22name%22%3A%22Cronos+Mainnet+Beta%22%2C%22chain%22%3A%22CRO%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fcronos-beta.rpc.thirdweb.com%2Fa8a7f8aa7ddd8115bad1892b179be99f%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Cronos%22%2C%22symbol%22%3A%22CRO%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22cro%22%2C%22chainId%22%3A25%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22cronos-beta%22%7D&tokenId=0&theme=dark&primaryColor=cyan"
